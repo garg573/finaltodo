9d9a12531c0919a1ff7323ca20f9542fb7a87298
garg573 <gauravgarg573@gmail.com>
2022-08-17 18:12:52 +0530

    appp

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
-# from urllib import request
-from flask import Flask,render_template,url_for,request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-
-app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.String(200),nullable=True)  
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
@@ -30,10 +26,11 @@ def index():
             db.session.commit()
             return redirect('/')
         except:
-            return 'There is some issue in adding your task'
+            return 'There was an issue adding your task'
+
     else:
-        tasks=Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html',tasks=tasks)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 @app.route('/delete/<int:id>')
@@ -45,12 +42,11 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'there was some problem in adding'
+        return 'There was a problem deleting that task'
 
-
-@app.route('/update/<int:id>',methods=['GET','POST'])
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task =Todo.query.get_or_404(id)
+    task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
@@ -59,9 +55,11 @@ def update(id):
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was problem in adding'
-    else:
-        return render_template('update.html',task=task)
+            return 'There was an issue updating your task'
 
-if __name__=='__main__':
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
